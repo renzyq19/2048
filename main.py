b0260d90077d1fb30bb479023f763270e1936871
Will de Renzy-Martin <will.derenzymartin@gmail.com>
2021-03-23 14:37:24 +0000

    Fix merge conflict

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-
-def main(): 
-  pass
-
-if __name__ == '__main__':
-<<<<<<< HEAD
-  main()
-
 def main():
   grid = [
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0],
     [0,0,0,0]]
-=======
+  
+if __name__ == '__main__':
   main()
->>>>>>> b6eeebb5a60de34564c17053f716f9e98aa30647
